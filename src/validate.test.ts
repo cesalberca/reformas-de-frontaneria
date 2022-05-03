de167bbd54ec770840b2a61ee509fabf5e559079
César Alberca <cesalberca@gmail.com>
2022-05-03 18:25:44 +0200

    Improve algorithm

--- src/validate.test.ts
+++ src/validate.test.ts
@@ -6,4 +6,16 @@ describe('validate', () => {
 
     expect(actual).toEqual([-1, -1, -1])
   })
+
+  it('should validate that some letters are present in the word but not in the particular position', () => {
+    const actual = validate('abc', 'dea')
+
+    expect(actual).toEqual([0, -1, -1])
+  })
+
+  it('should that a letter is present in the same position as the guessed word', () => {
+    const actual = validate('abc', 'ade')
+
+    expect(actual).toEqual([1, -1, -1])
+  })
 })
---
+++ src/validate.v5.ts
@@ -0,0 +1,26 @@
+export function validate(wordToTry: string, wordToGuess: string) {
+  let result = []
+
+  if (wordToTry === wordToGuess) {
+    return Array.from({ length: wordToGuess.length }).fill(1)
+  }
+
+  /**
+   *  1 → Guessed letter and position correctly
+   *  0 → Guessed letter correctly
+   * -1 → Miss
+   */
+  for (let i = 0; i < wordToTry.length; i++) {
+    if (wordToTry[i] === wordToGuess[i]) {
+      result.push(1)
+    } else if (wordToGuess.indexOf(wordToTry[i]) !== -1) {
+      result.push(0)
+    } else {
+      result.push(-1)
+    }
+  }
+
+  return result
+}
+
+export {}
