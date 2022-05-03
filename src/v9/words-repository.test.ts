3509756d5fabde1b3357d627a877c9111ce005c2
César Alberca <cesalberca@gmail.com>
2022-05-03 19:11:55 +0200

    Fix test

--- src/v9/words-repository.test.ts
+++ src/v9/words-repository.test.ts
@@ -1,12 +1,14 @@
 import { WordsRepository } from './words-repository'
-import { anything, instance, mock, when } from 'ts-mockito'
+import { instance, mock, when } from 'ts-mockito'
 
 describe('WordsRepository', () => {
   it('should find all words', async () => {
-    const response = mock<Response>()
-    when(response.json()).thenResolve(['foo', 'bar', 'baz'])
     const global = mock<typeof globalThis>()
-    when(global.fetch(anything())).thenResolve(instance(response))
+    when(
+      global.fetch(
+        'https://gist.githubusercontent.com/cesalberca/3d0e343842c7697e0f788178e59fb471/raw/22bfe35c910dd02db78fec87fa39ca25cc604816/data.json',
+      ),
+    ).thenResolve({ json: () => Promise.resolve(['foo', 'bar', 'baz']) } as Response)
     const wordsRepository = new WordsRepository(instance(global))
 
     const actual = await wordsRepository.findAll()
--- src/v9/words-repository.ts
+++ src/v9/words-repository.ts
@@ -4,14 +4,10 @@ export class WordsRepository {
   constructor(private readonly global: typeof globalThis) {}
 
   findAll(): Promise<Word[]> {
-    console.log(0)
     return this.global
       .fetch(
         'https://gist.githubusercontent.com/cesalberca/3d0e343842c7697e0f788178e59fb471/raw/22bfe35c910dd02db78fec87fa39ca25cc604816/data.json',
       )
-      .then(x => {
-        console.log({ x })
-        return x.json()
-      })
+      .then(x => x.json())
   }
 }
