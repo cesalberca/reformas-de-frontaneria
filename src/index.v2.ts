a50d676de9644b562e1a8cf99b97051c42e70600
César Alberca <cesalberca@gmail.com>
2022-05-03 18:14:12 +0200

    Improve naming

---
+++ src/index.v2.ts
@@ -0,0 +1,47 @@
+fetch(
+  'https://gist.githubusercontent.com/cesalberca/3d0e343842c7697e0f788178e59fb471/raw/22bfe35c910dd02db78fec87fa39ca25cc604816/data.json',
+)
+  .then(x => x.json())
+  .then(availableWords => {
+    const wordToGuess = availableWords[Math.floor(Math.random() * availableWords.length)]
+    console.log({ wordToGuess })
+    const form = document.querySelector('#form')
+    const element = document.querySelector('#input')
+
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      let result = []
+
+      // @ts-ignore
+      if (element.value === wordToGuess) {
+        let x = []
+        for (let i = 0; i < wordToGuess.length; i++) {
+          x.push(1)
+        }
+        result = x
+      } else {
+        /**
+         *  1 → Guessed letter and position correctly
+         *  0 → Guessed letter correctly
+         * -1 → Miss
+         */
+        // @ts-ignore
+        for (let i = 0; i < element.value.length; i++) {
+          // @ts-ignore
+          if (element.value[i] === wordToGuess[i]) {
+            result.push(1)
+            // @ts-ignore
+            // } else if (word.includes(element.value[i])) {
+          } else if (wordToGuess.indexOf(element.value[i]) !== -1) {
+            result.push(0)
+          } else {
+            result.push(-1)
+          }
+        }
+      }
+
+      console.log({ result })
+    })
+  })
+
+export {}
