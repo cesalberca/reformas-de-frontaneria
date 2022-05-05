92641ffe6720d50f364f30838f091b585ff09c59
César Alberca <cesalberca@gmail.com>
2022-05-05 17:59:46 +0100

    Extract into class

--- src/v9/index.ts
+++ src/v9/index.ts
@@ -1,10 +1,12 @@
 import { WordsRepository } from './words-repository'
 import { GetWordGuessesUseCase } from './get-word-guesses-use-case'
 import { GetRandomWordToGuessUseCase } from './get-random-word-to-guess-use-case'
+import { WordValidator } from './validate'
 
-const wordsRepository = new WordsRepository(window)
 const seed = Math.random()
-const getWordGuessesUseCase = new GetWordGuessesUseCase()
+const wordsRepository = new WordsRepository(window)
+const wordValidator = new WordValidator()
+const getWordGuessesUseCase = new GetWordGuessesUseCase(wordValidator)
 const getRandomWordToGuessUseCase = new GetRandomWordToGuessUseCase(wordsRepository, () => seed)
 
 const form = document.querySelector<HTMLFormElement>('#form')
