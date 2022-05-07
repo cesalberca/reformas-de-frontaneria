504908759c1ce6eda3b0a818f4e832440df26a95
Nya Garcia <nyablk97@gmail.com>
2022-05-07 02:48:26 +0200

    refactor(v3): extract to smaller functions

--- src/v3-functions/index.ts
+++ src/v3-functions/index.ts
@@ -1,20 +1,34 @@
 import { validate } from './validate'
 
+let wordToGuess = ''
+
 fetch(
   'https://gist.githubusercontent.com/cesalberca/3d0e343842c7697e0f788178e59fb471/raw/22bfe35c910dd02db78fec87fa39ca25cc604816/data.json',
 )
   .then(x => x.json())
   .then(availableWords => {
-    const wordToGuess = availableWords[Math.floor(Math.random() * availableWords.length)]
+    wordToGuess = getRandomWord(availableWords)
     console.log({ wordToGuess })
-    const form = document.querySelector<HTMLFormElement>('#form')!
-    const element = document.querySelector<HTMLInputElement>('#input')!
 
-    form.addEventListener('submit', e => {
-      e.preventDefault()
-      const result = validate(element.value, wordToGuess)
-      console.log({ result })
-    })
+    addFormListeners()
   })
+
+function getRandomWord(availableWords: string[]) {
+  return availableWords[Math.floor(Math.random() * availableWords.length)]
+}
+
+function addFormListeners() {
+  const form = document.querySelector<HTMLFormElement>('#form')!
+
+  form.addEventListener('submit', validateWord)
+}
+
+const validateWord = (e: Event) => {
+  const element = document.querySelector<HTMLInputElement>('#input')!
+
+  e.preventDefault()
+  const result = validate(element.value, wordToGuess)
+  console.log({ result })
+}
 
 export {}
--- src/v3-functions/validate.ts
+++ src/v3-functions/validate.ts
@@ -1,30 +1,47 @@
 export function validate(wordToTry: string, wordToGuess: string) {
-  let result = []
+  return isWordGuessed(wordToTry, wordToGuess)
+    ? setCorrectWordResult(wordToGuess)
+    : setIndividualLetterResult(wordToTry, wordToGuess)
+}
 
-  if (wordToTry === wordToGuess) {
-    let x = []
-    for (let i = 0; i < wordToGuess.length; i++) {
-      x.push(1)
-    }
-    result = x
-  } else {
-    /**
-     *  1 → Guessed letter and position correctly
-     *  0 → Guessed letter correctly
-     * -1 → Miss
-     */
-    for (let i = 0; i < wordToTry.length; i++) {
-      if (wordToTry[i] === wordToGuess[i]) {
-        result.push(1)
-      } else if (wordToGuess.indexOf(wordToTry[i]) !== -1) {
-        result.push(0)
-      } else {
-        result.push(-1)
-      }
+function isWordGuessed(wordToTry: string, wordToGuess: string) {
+  return wordToTry === wordToGuess
+}
+
+function setCorrectWordResult(wordToGuess: string) {
+  let x = []
+  for (let i = 0; i < wordToGuess.length; i++) {
+    x.push(1)
+  }
+  return x
+}
+
+function setIndividualLetterResult(wordToTry: string, wordToGuess: string) {
+  let result = []
+  /**
+   *  1 → Guessed letter and position correctly
+   *  0 → Guessed letter correctly
+   * -1 → Miss
+   */
+  for (let i = 0; i < wordToTry.length; i++) {
+    if (lettersAreEqual(wordToTry[i], wordToGuess[i])) {
+      result.push(1)
+    } else if (wordIncludesLetter(wordToGuess, wordToTry[i])) {
+      result.push(0)
+    } else {
+      result.push(-1)
     }
   }
 
   return result
+}
+
+function lettersAreEqual(firstLetter: string, secondLetter: string) {
+  return firstLetter === secondLetter
+}
+
+function wordIncludesLetter(wordToGuess: string, letter: string) {
+  return wordToGuess.indexOf(letter) !== -1
 }
 
 export {}
