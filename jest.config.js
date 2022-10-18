fbbb94c27a4cbd5cc664173d1349d59ddd8bebea
cesalberca <cesalberca@gmail.com>
2022-10-18 23:25:36 +0200

    Improve jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   coverageDirectory: 'coverage',
   coverageReporters: ['text-summary', 'lcov'],
+  collectCoverageFrom: ['./src/v8-mvc/**/*.{js,ts}', '!./src/v8-mvc/**/index.{js,ts}'],
 }
